6709eca46efb1e4495019b7452c97f401d521946
TeemuBergman <teemu.bergman@helsinki.fi>
2025-03-09 19:37:20 +0200

    Added correct filepath for testing

--- backend/main.py
+++ backend/main.py
@@ -2,5 +2,5 @@ from demodata_server.server import DemodataServer
 
 if __name__ == "__main__":
     demodata_server = DemodataServer("0.0.0.0", 8080, "/demodata")
-    demodata_server.demodata_input("filename.json")
+    demodata_server.demodata_input("./data/test_large.json")
     demodata_server.start_server()
